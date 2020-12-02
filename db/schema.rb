ee322a5fb5b0c7b80bd0ef426beea9fcd3a9b7c3
ianfletcher55 <ian.fletcher55@gmail.com>
2020-12-02 15:14:06 -0600

    converts database to postgresql and adds seeds file with dummy data

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_11_19_032708) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "memes", force: :cascade do |t|
     t.string "image"
     t.string "top_text"
